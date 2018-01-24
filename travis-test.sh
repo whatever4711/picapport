e73ebf17e3bd922ecca887cf7b42acc4a59a6b4c
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2018-01-24 14:09:29 +0100

    Updated Image Names

--- travis-test.sh
+++ travis-test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
+if [[ "$TRAVIS_TAG" ]]; then
+  tag=$TRAVIS_TAG
+else
+  tag=latest
+fi
+
+image="whatever4711/picapport"
+docker push "$image:linux-$ARCH-$tag"
+
 if [ "$ARCH" == "amd64" ]; then
   # test image
-  docker run -d -p 8080:8080 --name=picapporttest picapport
+  docker run -d -p 8080:8080 --name=picapporttest "$image:linux-$ARCH-$tag"
 
   sleep 5
 
