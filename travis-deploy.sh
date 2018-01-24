d85db56c1f18e684b410d022d3915af27937d841
Marcel Großmann <whatever4711@gmail.com>
2018-01-24 20:40:24 +0100

    Push with make

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -8,22 +8,20 @@ else
 fi
 
 image="$REPO"
-docker push "$image:linux-$ARCH-$tag"
 
+echo "Downloading docker client with manifest command"
+wget https://6582-88013053-gh.circle-artifacts.com/1/work/build/docker-linux-amd64
+mv docker-linux-amd64 docker
+chmod +x docker
+./docker version
 
-  echo "Downloading docker client with manifest command"
-  wget https://6582-88013053-gh.circle-artifacts.com/1/work/build/docker-linux-amd64
-  mv docker-linux-amd64 docker
-  chmod +x docker
-  ./docker version
+set -x
 
-  set -x
-
-  echo "Pushing manifest $image:$tag"
-  ./docker -D manifest create "$image:$tag" \
-    "$image:linux-amd64-$tag" \
-    "$image:linux-arm32v6-$tag" \
-    "$image:linux-arm64v8-$tag"
-  ./docker manifest annotate "$image:$tag" "$image:linux-arm32v6-$tag" --os linux --arch arm --variant v6
-  ./docker manifest annotate "$image:$tag" "$image:linux-arm64v8-$tag" --os linux --arch arm64 --variant v8
-  ./docker manifest push "$image:$tag"
+echo "Pushing manifest $image:$tag"
+./docker -D manifest create "$image:$tag" \
+  "$image:linux-amd64-$tag" \
+  "$image:linux-arm32v6-$tag" \
+  "$image:linux-arm64v8-$tag"
+./docker manifest annotate "$image:$tag" "$image:linux-arm32v6-$tag" --os linux --arch arm --variant v6
+./docker manifest annotate "$image:$tag" "$image:linux-arm64v8-$tag" --os linux --arch arm64 --variant v8
+./docker manifest push "$image:$tag"
