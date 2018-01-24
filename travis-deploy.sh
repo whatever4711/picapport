680511261d557184ed9b97b2af9c81f6d97c6291
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2018-01-24 11:36:42 +0100

    Udpated Deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 set -e
 
+if [[ "$TRAVIS_TAG" ]]; then
+  tag=$TRAVIS_TAG
+else
+  tag=latest
+fi
+
 image="whatever4711/picapport"
-docker tag picapport "$image:linux-$ARCH-$TRAVIS_TAG"
-docker push "$image:linux-$ARCH-$TRAVIS_TAG"
+docker tag picapport "$image:linux-$ARCH-$tag"
+docker push "$image:linux-$ARCH-$tag"
 
 if [ "$ARCH" == "amd64" ]; then
   set +e
-  echo "Waiting for other images $image:linux-arm-$TRAVIS_TAG"
-  until docker run --rm stefanscherer/winspector "$image:linux-arm-$TRAVIS_TAG"
+  echo "Waiting for other images $image:linux-arm-$tag"
+  until docker run --rm stefanscherer/winspector "$image:linux-arm-$tag"
   do
     sleep 15
     echo "Try again"
   done
-  until docker run --rm stefanscherer/winspector "$image:linux-arm64-$TRAVIS_TAG"
+  until docker run --rm stefanscherer/winspector "$image:linux-arm64-$tag"
   do
     sleep 15
     echo "Try again"
@@ -28,21 +34,12 @@ if [ "$ARCH" == "amd64" ]; then
 
   set -x
 
-  echo "Pushing manifest $image:$TRAVIS_TAG"
-  ./docker -D manifest create "$image:$TRAVIS_TAG" \
-    "$image:linux-amd64-$TRAVIS_TAG" \
-    "$image:linux-arm-$TRAVIS_TAG" \
-    "$image:linux-arm64-$TRAVIS_TAG"
-  ./docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm --variant v6
-  ./docker manifest annotate "$image:$TRAVIS_TAG" "$image:linux-arm64-$TRAVIS_TAG" --os linux --arch arm64 --variant v8
-  ./docker manifest push "$image:$TRAVIS_TAG"
-
-  echo "Pushing manifest $image:latest"
-  ./docker -D manifest create "$image:latest" \
-    "$image:linux-amd64-$TRAVIS_TAG" \
-    "$image:linux-arm-$TRAVIS_TAG" \
-    "$image:linux-arm64-$TRAVIS_TAG"
-  ./docker manifest annotate "$image:latest" "$image:linux-arm-$TRAVIS_TAG" --os linux --arch arm --variant v6
-  ./docker manifest annotate "$image:latest" "$image:linux-arm64-$TRAVIS_TAG" --os linux --arch arm64 --variant v8
-  ./docker manifest push "$image:latest"
+  echo "Pushing manifest $image:$tag"
+  ./docker -D manifest create "$image:$tag" \
+    "$image:linux-amd64-$tag" \
+    "$image:linux-arm-$tag" \
+    "$image:linux-arm64-$tag"
+  ./docker manifest annotate "$image:$tag" "$image:linux-arm-$tag" --os linux --arch arm --variant v6
+  ./docker manifest annotate "$image:$tag" "$image:linux-arm64-$tag" --os linux --arch arm64 --variant v8
+  ./docker manifest push "$image:$tag"
 fi
