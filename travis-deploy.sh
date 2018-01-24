64313981d97c0af6cb5fc9d92111ccc5fa84ce6a
Marcel Großmann <whatever4711@gmail.com>
2018-01-24 20:33:44 +0100

    Waiting for official stage builds with Travis

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -10,20 +10,6 @@ fi
 image="$REPO"
 docker push "$image:linux-$ARCH-$tag"
 
-if [ "$ARCH" == "amd64" ]; then
-  set +e
-  echo "Waiting for other images $image:linux-arm-$tag"
-  until docker run --rm stefanscherer/winspector "$image:linux-arm32v6-$tag"
-  do
-    sleep 15
-    echo "Try again"
-  done
-  until docker run --rm stefanscherer/winspector "$image:linux-arm64v8-$tag"
-  do
-    sleep 15
-    echo "Try again"
-  done
-  set -e
 
   echo "Downloading docker client with manifest command"
   wget https://6582-88013053-gh.circle-artifacts.com/1/work/build/docker-linux-amd64
@@ -41,4 +27,3 @@ if [ "$ARCH" == "amd64" ]; then
   ./docker manifest annotate "$image:$tag" "$image:linux-arm32v6-$tag" --os linux --arch arm --variant v6
   ./docker manifest annotate "$image:$tag" "$image:linux-arm64v8-$tag" --os linux --arch arm64 --variant v8
   ./docker manifest push "$image:$tag"
-fi
