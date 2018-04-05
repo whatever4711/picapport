49c757df0bea1dd658af3cb83015e0af3ead804b
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2018-04-05 17:35:58 +0200

    Trying Windows Build

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
-echo $REPO
+echo %REPO%
 
 ([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 
 
-docker build -t $picapport --build-arg --build-arg BUILD_DATE=%([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")%  -f Dockerfile.windows .
+docker build -t %REPO% --build-arg BUILD_DATE=%([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")%  -f Dockerfile.windows .
