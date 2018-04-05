79b8622de51e995f89b1b7c2d81302b3cd8e30ca
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2018-04-05 17:51:42 +0200

    Trying Windows

--- build.ps1
+++ build.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = 'Stop';
 
-echo $env:REPO`:windows-amd64
-
-([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
+set DATE=([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 
 
-docker build -t $env:REPO --build-arg BUILD_DATE=%([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")% -f Dockerfile.windows .
+docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=%DATE% -f Dockerfile.windows .
