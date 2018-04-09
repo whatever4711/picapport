ff714495e5cee095ede443f9623e14f6c947c71a
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2018-04-09 09:23:50 +0200

    Pushing Windows Container

--- build.ps1
+++ build.ps1
@@ -4,3 +4,7 @@ set DATE=([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 
 
 docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=%DATE% -f Dockerfile.windows .
+
+docker login -u $env:DOCKER_USER -p $env:DOCKER_PASS
+
+docker push $env:REPO`:windows-amd64
