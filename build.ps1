45431a38e7f27f30cf081bbcfc9636b29c5073e5
Marcel Großmann <whatever4711@gmail.com>
2018-04-09 09:38:54 +0200

    Updates

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop';
-
+set VERSION=(type VERSION)
 set DATE=([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 
 
-docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=%DATE% -f Dockerfile.windows .
+docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=%DATE% --build-arg VERSION=%VERSION% -f Dockerfile.windows .
 
 docker login -u $env:DOCKER_USER -p $env:DOCKER_PASS
 
