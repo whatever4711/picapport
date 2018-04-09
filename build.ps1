790fbbee2bfd68f9e09fa91a5ac407e0974759b9
Marcel Großmann <whatever4711@gmail.com>
2018-04-09 13:02:33 +0200

    Variables

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop';
-$VER=(type VERSION)
+$VERSION=(type VERSION)
 $DATE=([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 
 echo "Building Picapport $VER at $DATE"
 
-docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=%DATE% --build-arg VERSION=%VER% -f Dockerfile.windows .
+docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION -f Dockerfile.windows .
 
 docker login -u $env:DOCKER_USER -p $env:DOCKER_PASS
 docker push $env:REPO`:windows-amd64
