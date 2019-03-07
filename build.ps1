b030468f7a3cb768a49d64c4aecd9b8e6b10fe63
Marcel Großmann <whatever4711@gmail.com>
2019-03-07 13:14:19 +0100

    Added Qemu Arch

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $VERSION=(type VERSION)
 $IMAGE = "alpine:latest"
+$QEMU_ARCH = $env:ARCH -replace 'arm32.*','arm' -replace 'arm64.*','aarch64' -replace 'amd64','x86_64'
 $VERSION=$VERSION -replace '\.','-'
 $DATE=([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 
@@ -12,7 +13,7 @@ if ($isWindows){
   docker build -t $env:REPO --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION -f Dockerfile.windows .
 } else {
   docker run --rm --privileged "multiarch/qemu-user-static:register" --reset
-  docker build -t $env:REPO --build-arg IMAGE="$env:ARCH/$IMAGE" --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION --build-arg "ARCH=$env:ARCH" -f Dockerfile .
+  docker build -t $env:REPO --build-arg IMAGE="$env:ARCH/$IMAGE" --build-arg QEMU=$QEMU_ARCH --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION --build-arg "ARCH=$env:ARCH" -f Dockerfile .
 }
 
 docker images
