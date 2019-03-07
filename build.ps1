4fa375d7e463b1d29870cd2039b6ef32063af9cb
Marcel Großmann <whatever4711@gmail.com>
2019-03-07 11:29:17 +0100

    Appveyor STDIN

--- build.ps1
+++ build.ps1
@@ -7,6 +7,6 @@ echo "Building Picapport $VER at $DATE"
 
 docker build -t $env:REPO`:windows-amd64 --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION -f Dockerfile.windows .
 
-echo | set /p="$env:DOCKER_PASS" | docker login -u $env:DOCKER_USER --password-stdin
+echo $env:DOCKER_PASS | docker login -u $env:DOCKER_USER --password-stdin
 docker push $env:REPO`:windows-amd64
 docker logout
