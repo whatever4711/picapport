918166297580d29b2d538288a59f7cba744fdfec
Marcel Großmann <whatever4711@gmail.com>
2019-03-08 14:23:09 +0100

    With retry

--- deploy.ps1
+++ deploy.ps1
@@ -62,7 +62,7 @@ $auth64 = [Convert]::ToBase64String($auth)
 
 $os = If ($isWindows) {"windows"} Else {"linux"}
 docker tag picapport "$($image):$os-$env:ARCH-$TAG"
-Retry-Command -Command 'docker push "$($image):$os-$env:ARCH-$TAG"' -Args @{ } -Verbose
+Retry-Command -Command 'docker push "$($image):$os-$env:ARCH-$TAG"' -Args @{ name="$($image):$os-$env:ARCH-$TAG" } -Verbose
 
 if ($isWindows) {
   # Windows
