c71a85f76136ff59162767b3b544f01945e67548
Marcel Großmann <whatever4711@gmail.com>
2019-03-11 10:29:25 +0100

    7.4.00

--- deploy.ps1
+++ deploy.ps1
@@ -54,66 +54,66 @@ $auth64 = [Convert]::ToBase64String($auth)
 "@ | Out-File -Encoding Ascii ~/.docker/config.json
 
 $os = If ($isWindows) {"windows"} Else {"linux"}
-docker tag picapport "$($image):$os-$env:ARCH-$TAG"
-Retry-Command -Command "docker push $($image):$os-$env:ARCH-$TAG" -Verbose
+docker tag picapport "$($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME"
+Retry-Command -Command "docker push $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME" -Verbose
 
 if ($isWindows) {
   # Windows
   Write-Host "Rebasing image to produce 1709 variant"
   npm install -g rebase-docker-image
   Retry-Command -Command "rebase-docker-image `
-    $($image):$os-$env:ARCH-$TAG `
-    -t $($image):$os-$env:ARCH-$TAG-1709 `
+    $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME `
+    -t $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME-1709 `
     -b microsoft/nanoserver:1709" -Verbose
 
   Write-Host "Rebasing image to produce 1803 variant"
   npm install -g rebase-docker-image
   Retry-Command -Command "rebase-docker-image `
-    $($image):$os-$env:ARCH-$TAG `
-    -t $($image):$os-$env:ARCH-$TAG-1803 `
+    $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME `
+    -t $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME-1803 `
     -b microsoft/nanoserver:1803" -Verbose
 
   Write-Host "Rebasing image to produce 1809 variant"
   npm install -g rebase-docker-image
   Retry-Command -Command "rebase-docker-image `
-    $($image):$os-$env:ARCH-$TAG `
+    $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME `
     -s microsoft/nanoserver:sac2016 `
-    -t $($image):$os-$env:ARCH-$TAG-1809 `
+    -t $($image):$os-$env:ARCH-$env:APPVEYOR_REPO_TAG_NAME-1809 `
     -b stefanscherer/nanoserver:10.0.17763.253" -Verbose
 
 } else {
   # Linux
   if ($env:ARCH -eq "s390x") {
     # The last in the build matrix
-    docker -D manifest create "$($image):$TAG" `
-      "$($image):linux-amd64-$TAG" `
-      "$($image):linux-i386-$TAG" `
-      "$($image):linux-arm32v6-$TAG" `
-      "$($image):linux-arm64v8-$TAG" `
-      "$($image):linux-ppc64le-$TAG" `
-      "$($image):linux-s390x-$TAG" `
-      "$($image):windows-amd64-$TAG" `
-      "$($image):windows-amd64-$TAG-1709" `
-      "$($image):windows-amd64-$TAG-1803" `
-      "$($image):windows-amd64-$TAG-1809"
-    docker manifest annotate "$($image):$TAG" "$($image):linux-arm32v6-$TAG" --os linux --arch arm --variant v6
-    docker manifest annotate "$($image):$TAG" "$($image):linux-arm64v8-$TAG" --os linux --arch arm64 --variant v8
-    Retry-Command -Command "docker manifest push $($image):$TAG" -Verbose
+    docker -D manifest create "$($image):$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-i386-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-arm32v6-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-ppc64le-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-s390x-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME-1709" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME-1803" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME-1809"
+    docker manifest annotate "$($image):$env:APPVEYOR_REPO_TAG_NAME" "$($image):linux-arm32v6-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm --variant v6
+    docker manifest annotate "$($image):$env:APPVEYOR_REPO_TAG_NAME" "$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm64 --variant v8
+    Retry-Command -Command "docker manifest push $($image):$env:APPVEYOR_REPO_TAG_NAME" -Verbose
 
     Write-Host "Pushing manifest $($image):latest"
     docker -D manifest create "$($image):latest" `
-      "$($image):linux-amd64-$TAG" `
-      "$($image):linux-i386-$TAG" `
-      "$($image):linux-arm32v6-$TAG" `
-      "$($image):linux-arm64v8-$TAG" `
-      "$($image):linux-ppc64le-$TAG" `
-      "$($image):linux-s390x-$TAG" `
-      "$($image):windows-amd64-$TAG" `
-      "$($image):windows-amd64-$TAG-1709" `
-      "$($image):windows-amd64-$TAG-1803" `
-      "$($image):windows-amd64-$TAG-1809"
-    docker manifest annotate "$($image):latest" "$($image):linux-arm32v6-$TAG" --os linux --arch arm --variant v6
-    docker manifest annotate "$($image):latest" "$($image):linux-arm64v8-$TAG" --os linux --arch arm64 --variant v8
+      "$($image):linux-amd64-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-i386-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-arm32v6-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-ppc64le-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):linux-s390x-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME-1709" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME-1803" `
+      "$($image):windows-amd64-$env:APPVEYOR_REPO_TAG_NAME-1809"
+    docker manifest annotate "$($image):latest" "$($image):linux-arm32v6-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm --variant v6
+    docker manifest annotate "$($image):latest" "$($image):linux-arm64v8-$env:APPVEYOR_REPO_TAG_NAME" --os linux --arch arm64 --variant v8
     Retry-Command -Command "docker manifest push $($image):latest" -Verbose
 
     Invoke-WebRequest -Uri ("https://hooks.microbadger.com/images/whatever4711/picapport/h54qEvKKiyj8evp5FLbwRCouqks=") -Method POST
