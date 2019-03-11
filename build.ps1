e3b723f939cdf2e5da4c8340ab5fcb2a7a7e13f9
Marcel Großmann <whatever4711@gmail.com>
2019-03-11 13:45:58 +0100

    With VCS_REF

--- build.ps1
+++ build.ps1
@@ -1,18 +1,18 @@
 $ErrorActionPreference = 'Stop'
-$VERSION=type VERSION
-$VERSION=$VERSION -replace '\.','-'
+$VERSION = type VERSION
+$VERSION = $VERSION -replace '\.','-'
 $IMAGE = "alpine:latest"
 $QEMU_ARCH = $env:ARCH -replace 'arm32.*','arm' -replace 'arm64.*','aarch64' -replace 'amd64','x86_64'
-$DATE=([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
-$VCS_REF=git rev-parse --short HEAD
-$VCS_URL=git config --get remote.origin.url
+$DATE = ([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
+$VCS_REF = git rev-parse --short HEAD
+$VCS_URL = git config --get remote.origin.url
 
 Write-Host Starting build
 
 echo "Building Picapport v$VERSION at $DATE"
 
 if ($isWindows){
-  docker build -t picapport --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION --build-arg VCS_REF=$VCS_REF --build-arg VCS_URL=$VCS_URL Dockerfile.windows .
+  docker build -t picapport --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION --build-arg VCS_REF=$VCS_REF --build-arg VCS_URL=$VCS_URL -f Dockerfile.windows .
 } else {
   docker run --rm --privileged "multiarch/qemu-user-static:register" --reset
   docker build -t picapport --build-arg IMAGE="$env:ARCH/$IMAGE" --build-arg QEMU=$QEMU_ARCH --build-arg BUILD_DATE=$DATE --build-arg VERSION=$VERSION --build-arg "ARCH=$env:ARCH" --build-arg VCS_REF=$VCS_REF --build-arg VCS_URL=$VCS_URL -f Dockerfile .
