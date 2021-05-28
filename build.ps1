7b9ad8dec523f9bec5e8d1893d99c7c2cbf3279a
Marcel Großmann <whatever4711@gmail.com>
2021-05-28 09:43:42 +0200

    Logging in to Docker for Build Process

--- build.ps1
+++ build.ps1
@@ -7,6 +7,23 @@ $DATE = ([datetime]::now).toString("yyyy-MM-ddTHH:mm:ssZ")
 $VCS_REF = git rev-parse --short HEAD
 $VCS_URL = git config --get remote.origin.url
 
+Write-Host Starting deploy
+if (!(Test-Path ~/.docker)) { mkdir ~/.docker }
+# "$env:DOCKER_PASS" | docker login --username "$env:DOCKER_USER" --password-stdin
+# docker login with the old config.json style that is needed for manifest-tool
+$auth =[System.Text.Encoding]::UTF8.GetBytes("$($env:DOCKER_USER):$($env:DOCKER_PASS)")
+$auth64 = [Convert]::ToBase64String($auth)
+@"
+{
+  "auths": {
+    "https://index.docker.io/v1/": {
+      "auth": "$auth64"
+    }
+  },
+  "experimental": "enabled"
+}
+"@ | Out-File -Encoding Ascii ~/.docker/config.json
+
 Write-Host Starting build
 
 echo "Building Picapport v$VERSION at $DATE"
